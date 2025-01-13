d018900acc0652cbada76b03b2f0731beea68d95
leegeonhee0204@naver.com <leegeonhee0204@gmail.com>
2025-01-13 20:55:10 +0900

    UserRepositoryImpl::getQslUser에 대한 테스트케이스 구현

--- src/main/java/com/qsl/qsl_tutorial/boundedContext/user/entity/SiteUser.java
+++ src/main/java/com/qsl/qsl_tutorial/boundedContext/user/entity/SiteUser.java
@@ -3,12 +3,14 @@ package com.qsl.qsl_tutorial.boundedContext.user.entity;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 public class SiteUser {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
