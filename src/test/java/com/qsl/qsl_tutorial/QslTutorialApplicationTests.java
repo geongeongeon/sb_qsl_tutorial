1b37316f17137111bfe9c28d392c5e80e09807be
leegeonhee0204@naver.com <leegeonhee0204@gmail.com>
2025-01-13 20:17:31 +0900

    UserRepository 추가, 회원 추가 테스트 케이스

--- src/test/java/com/qsl/qsl_tutorial/QslTutorialApplicationTests.java
+++ src/test/java/com/qsl/qsl_tutorial/QslTutorialApplicationTests.java
@@ -1,13 +1,26 @@
 package com.qsl.qsl_tutorial;
 
+import com.qsl.qsl_tutorial.boundedContext.user.entity.SiteUser;
+import com.qsl.qsl_tutorial.boundedContext.user.repository.UserRepository;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Arrays;
 
 @SpringBootTest
 class QslTutorialApplicationTests {
+	@Autowired
+	private UserRepository userRepository;
 
 	@Test
-	void contextLoads() {
-	}
+	@DisplayName("회원 생성")
+	void t1() {
+		// {noop} : 비밀번호를 암호화하지 않고 그대로 사용
+		SiteUser u1 = new SiteUser(null, "user1", "{noop}1234", "user1@test.com");
+		SiteUser u2 = new SiteUser(null, "user2", "{noop}1234", "user2@test.com");
 
+		userRepository.saveAll(Arrays.asList(u1, u2));
+	}
 }
