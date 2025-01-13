bdf889c84de7676c92b814a1477863079d749e90
leegeonhee0204@naver.com <leegeonhee0204@gmail.com>
2025-01-13 20:21:22 +0900

    QueryDSL 로직용 클래스 UserRepositoryCustom, UserRepositoryImpl 추가

--- src/main/java/com/qsl/qsl_tutorial/boundedContext/user/repository/UserRepository.java
+++ src/main/java/com/qsl/qsl_tutorial/boundedContext/user/repository/UserRepository.java
@@ -3,5 +3,5 @@ package com.qsl.qsl_tutorial.boundedContext.user.repository;
 import com.qsl.qsl_tutorial.boundedContext.user.entity.SiteUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<SiteUser, Long> {
+public interface UserRepository extends JpaRepository<SiteUser, Long>, UserRepositoryCustom {
 }
